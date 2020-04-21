4b60c5c11fcb32b27fd64728afd7cdc0dd3bbf7d
Jevoun Tyrell <44033281+JTyrell@users.noreply.github.com>
2020-04-21 16:59:38 -0500

    Update db_data.py

--- app/db_data.py
+++ app/db_data.py
@@ -24,10 +24,10 @@ def getData():
 
 def getUser(idNumber):
     try:
-        connection = psycopg2.connect(user="Project1",
-                                    password="password",
-                                    host="localhost",
-                                    database="Project1")
+        connection = psycopg2.connect(user="foawmaqxxdiizl",
+                                    password="c04d6c9147e4b5429cb4d0c5f01a9e8999b447a6e724f8bba7ef54a02fded82a",
+                                    host="ec2-52-87-58-157.compute-1.amazonaws.com",
+                                    database="d6dvr900ccamgj")
         cursor = connection.cursor()
         postgreSQL_select_Query = "select * from user_profile where id="+idNumber
         cursor.execute(postgreSQL_select_Query)
